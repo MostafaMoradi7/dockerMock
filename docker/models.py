b78ff6847e176692eef323a24c101da54f3dad65
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2023-10-09 09:32:16 +0330

    Docker Config model created

--- docker/models.py
+++ docker/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
-# Create your models here.
+import subprocess
 
 
 class DockerConfigModel(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
     name = models.CharField(max_length=100)
-    image = models.CharField(max_length=200)
-    envs = models.JSONField()
+    image_address = models.CharField(max_length=200)
     command = models.CharField(max_length=300)
+    envs = models.JSONField()
+
+    def create_container_command(self):
+        try:
+            subprocess.check_call(self.command)
+
+            return True
+        except subprocess.CalledProcessError:
+            return False
