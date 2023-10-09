6a4271f1ebe4b7e1c66b1b87853d0d886fd905d6
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2023-10-09 09:34:13 +0330

    Docker config view created

--- docker/views.py
+++ docker/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import DockerConfigSerializer
 
-# Create your views here.
+
+class DockerConfigView(APIView):
+    def post(self, request):
+        serializer = DockerConfigSerializer(data=request.data)
+
+        if serializer.is_valid():
+            docker_config = serializer.save()
+            # command = docker_config.make_shell_command()
+            print(docker_config)
+            if docker_config.create_container_command():
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(
+                    serializer.errors, status=status.HTTP_400_BAD_REQUEST
+                )
+
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
